2cb66606b794870be546458bbe38ba874b82c4cd
Pradeep Kadiyala <pkadiya@us.ibm.com>
2025-01-15 15:47:44 -0600

    feat: Added boot volume to be 250GB. A loadbalancer (lb) with lb pool is created

--- solutions/quickstart/main.tf
+++ solutions/quickstart/main.tf
@@ -72,7 +72,7 @@ resource "ibm_is_image" "custom_image" {
 
 
 ##############################################################################
-# Create VPC
+# Create VPC and Subet
 ##############################################################################
 
 resource "ibm_is_vpc" "rhelai_vpc" {
@@ -80,32 +80,68 @@ resource "ibm_is_vpc" "rhelai_vpc" {
   resource_group          = module.resource_group.resource_group_id
 }
 
+resource "ibm_is_public_gateway" "rhelai_publicgateway" {
+  name                                      = "${var.prefix}-rhelai-gateway"
+  vpc                                       = ibm_is_vpc.rhelai_vpc.id
+  zone                                      = var.zone
+}
+
 resource "ibm_is_subnet" "rhelai_subnet" {
   name                                      = "${var.prefix}-rhelai-subnet"
   resource_group                            = module.resource_group.resource_group_id
   vpc                                       = ibm_is_vpc.rhelai_vpc.id
   zone                                      = var.zone
+  public_gateway                            = ibm_is_public_gateway.rhelai_publicgateway.id
   total_ipv4_address_count                  = 16
 }
 
-
 ##############################################################################
-# Create GPU Based VSI Instance using RHEL.ai Custom Image Image
+# Create instance template
 ##############################################################################
-
 
 resource "ibm_is_instance" "gpu_vsi" {
-  name                                    = "${var.prefix}-rhelai-instance"
+  name                                    = "${var.prefix}-instance"
   resource_group                          = module.resource_group.resource_group_id
   image                                   = ibm_is_image.custom_image.id
   profile                                 = local.gpu_machine_type    
-  keys                                    = [ibm_is_ssh_key.rhelai_ssh_key.id]
   vpc                                     = ibm_is_vpc.rhelai_vpc.id
   zone                                    = var.zone
+  keys                                    = [ibm_is_ssh_key.rhelai_ssh_key.id]
   primary_network_interface {
     subnet                                = ibm_is_subnet.rhelai_subnet.id
   }
-  boot_volume {
+  boot_volume {        
     size                                  = 250
   }
+}
+
+##############################################################################
+# Create load balancer
+##############################################################################
+
+resource "ibm_is_lb" "lb_app" {
+  name                                      = "${var.prefix}-lb"
+  resource_group                            = module.resource_group.resource_group_id
+  subnets                                   = [ ibm_is_subnet.rhelai_subnet.id ]
+  type                                      = "public"
+}
+
+resource "ibm_is_lb_pool" "lb_pool" {
+  name           = "${var.prefix}-lb-pool"
+  lb             = ibm_is_lb.lb_app.id
+  algorithm      = "round_robin"
+  protocol       = "http"
+  health_delay   = 60
+  health_retries = 5
+  health_timeout = 30
+  health_type    = "http"
+  proxy_protocol = "v1"
+}
+
+resource "ibm_is_lb_pool_member" "lb_pool_member" {
+  lb        = ibm_is_lb.lb_app.id
+  pool      = ibm_is_lb_pool.lb_pool.pool_id
+  port      = 8000
+  target_id = ibm_is_instance.gpu_vsi_template.id
+  weight    = 100
 }
