3fe9f7fdc699402d286c23d52617896333cf49b3
pradeepkadiyala80 <78549799+pradeepkadiyala80@users.noreply.github.com>
2025-03-24 16:50:50 -0500

    feat: reduce the number of variables by removing resource_group (#40)
    * feat: Choose the resource group. If NULL is provided then generate a name else use the existing provided under resource_group_name. This will reduce the number of parameters
    
    * feat: Choose the resource group. If NULL is provided then generate a name else use the existing provided under resource_group_name. This will reduce the number of parameters
    
    * feat: Choose the resource group. If NULL is provided then generate a name else use the existing provided under resource_group_name. This will reduce the number of parameters
    
    * feat: Choose the resource group. If NULL is provided then generate a name else use the existing provided under resource_group_name. This will reduce the number of parameters
    
    * feat: Choose the resource group. If NULL is provided then generate a name else use the existing provided under resource_group_name. This will reduce the number of parameters
    
    * refactor: precommit whitespace fix
    
    * chore: cra scan config refactor
    
    ---------
    
    Co-authored-by: Todd Giguere <todd.giguere@ibm.com>

--- solutions/rhelai_vpc/variables.tf
+++ solutions/rhelai_vpc/variables.tf
@@ -13,20 +13,9 @@ variable "prefix" {
   description = "Prefix to append to all resources created by this example"
 }
 
-variable "resource_group" {
-  type        = string
-  description = "The name of a new resource group to provision resources in."
-  default     = null
-
-  validation {
-    condition     = var.resource_group != null || var.existing_resource_group != null
-    error_message = "You must supply either a new resource group name (resource_group) or an existing resource group name (existing_resource_group)."
-  }
-}
-
 variable "existing_resource_group" {
   type        = string
-  description = "The name of a existing resource group to provision resources in. Do not set if you fill resource_group"
+  description = "Select the name of a existing resource group or select NULL to create new resource group."
   default     = null
 }
 
